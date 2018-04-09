0afce5af8c350e0b0a8b40dbbc65a63ba1477278
JerseySnapper <deronblair@gmail.com>
2018-04-08 21:48:43 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    public float playerFloatForce = 300f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Vertical"))
+        {
+            myRigidBody.AddForce(transform.up * playerFloatForce);
+        }
+	}
+}
